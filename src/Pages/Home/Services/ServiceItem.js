6004bcc6866d58489ca63421dbfc206a362fbbb4
Medmly20208 <moulaymohamed856@gmail.com>
2022-10-18 19:47:19 +0100

    fixing more bugs

--- src/Pages/Home/Services/ServiceItem.js
+++ src/Pages/Home/Services/ServiceItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const ServiceItem = (props) => {
-  const rowDirection = props.index % 2 === 0 ? "-reverse" : " "; //make a section service in postion and the next service section in the opposite position
+  const rowDirection =
+    props.index % 2 === 0 ? " flex-row-reverse	" : " flex-row "; //make a section service in postion and the next service section in the opposite position
 
   const ElementStyle =
-    "flex flex-col justify-between items-center mt-20 gap-2 sx:flex-row" +
-    rowDirection;
+    "flex " +
+    rowDirection +
+    "justify-between items-center mt-20 gap-2 sf:flex-col";
 
-  console.log(ElementStyle);
   return (
     <div className={ElementStyle}>
       <div className="w-[100%] sx:w-[40%]">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
       sx: { min: "900px" }, //display components
+      sf: { max: "900px" },
     },
     extend: {},
   },
